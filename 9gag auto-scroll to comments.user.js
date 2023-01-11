50ff3eca87a6d192bcda5d4a150e1f3f16e9bf51
LostViking09 <117819350+LostViking09@users.noreply.github.com>
2023-01-11 11:27:30 +0100

    Add files via upload

---
+++ 9gag auto-scroll to comments.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         9gag auto-scroll to comments
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automagically shows you the comment section, instead of the new *related* one
+// @author       LostViking09
+// @match        https://9gag.com/gag/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=9gag.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var currentURL = window.location.href;
+
+    if (!currentURL.includes("#comment")){
+        location.assign(currentURL + "#comment");
+        }
+})();
